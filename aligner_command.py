d39fa2b14f569f0c3f2183aef9a9ce7a5e570917
generall <vasnetsov93@gmail.com>
2015-04-28 00:11:44 +0300

    add ruby check

--- aligner_command.py
+++ aligner_command.py
@@ -28,35 +28,39 @@ class AlignerCommand(sublime_plugin.TextCommand):
 				log(l)
 			log("---")
 
-			slave = Popen(['ruby', path + '/pipe_launch.rb', lang], stdin=PIPE, stdout=PIPE, stderr=STDOUT)
-
-			req_version = (2,7)
-			cur_version = (sys.version_info.major, sys.version_info.minor)
-
-			if cur_version > req_version:
-				slave.stdin.write(bytes(str(len(lines)) + "\n" , "UTF-8"));
-				slave.stdin.write(bytes(txt_lines       + "\n" , "UTF-8"));
-				slave.stdin.close()
-			else:
-				slave.stdin.write(bytes(str(len(lines)) + "\n" ));
-				slave.stdin.write(bytes(txt_lines       + "\n" ));
-				
-
-			log("patch: " + path)
-
-			result = []
-			while True:
-				# check if slave has terminated:
-				if slave.poll() is not None:
-					break
-				# read one line, remove newline chars and trailing spaces:
-				line = slave.stdout.readline().rstrip()
-				log(line)
-				result.append(line.decode("utf-8"))
-			responce = '\n'.join(result)
-			responce = responce.rstrip('\n')
-			self.view.replace(edit, lines_region, responce);
-			self.view.sel().clear()
+			try:
+				slave = Popen(['ruby', path + '/pipe_launch.rb', lang], stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+	
+				req_version = (2,7)
+				cur_version = (sys.version_info.major, sys.version_info.minor)
+	
+				if cur_version > req_version:
+					slave.stdin.write(bytes(str(len(lines)) + "\n" , "UTF-8"));
+					slave.stdin.write(bytes(txt_lines       + "\n" , "UTF-8"));
+					slave.stdin.close()
+				else:
+					slave.stdin.write(bytes(str(len(lines)) + "\n" ));
+					slave.stdin.write(bytes(txt_lines       + "\n" ));
+					
+	
+				log("patch: " + path)
+	
+				result = []
+				while True:
+					# check if slave has terminated:
+					if slave.poll() is not None:
+						break
+					# read one line, remove newline chars and trailing spaces:
+					line = slave.stdout.readline().rstrip()
+					log(line)
+					result.append(line.decode("utf-8"))
+				responce = '\n'.join(result)
+				responce = responce.rstrip('\n')
+				self.view.replace(edit, lines_region, responce);
+				self.view.sel().clear()
+			except OSError as e:
+				if e.errno == os.errno.ENOENT:
+					sublime.error_message("ruby is not installed")
 
 		os.chdir(prev_dir)
 
